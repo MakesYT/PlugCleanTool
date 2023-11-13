e305b9123d6625409165a9746cccc17fa9e8219c
MakesYT <42534870+MakesYT@users.noreply.github.com>
2023-11-13 21:23:16 +0800

    1
    1

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine($"插件名称{args[0]}");
+File.Delete($"{args[0]}.deps.json");
+File.Delete($"{args[0]}.runtimeconfig.json");
+File.Delete($"Microsoft.Windows.SDK.NET.dll");
+File.Delete($"WinRT.Runtime.dll");
