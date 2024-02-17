80c50646d0368f8b4b2326c2e466c910120fd0aa
MakesYT <42534870+MakesYT@users.noreply.github.com>
2024-02-17 13:43:04 +0800

    1

--- Program.cs
+++ Program.cs
@@ -10,12 +10,12 @@ if (dir.Exists)
     foreach (var file in dir.GetDirectories())
     {
         Console.WriteLine($"插件名称{file.Name}");
-        var path = $"{file.FullName}\\{file.Name}.deps.json";
+        var path = $"{file.FullName}{Path.DirectorySeparatorChar}{file.Name}.deps.json";
         Console.WriteLine(path);
         File.Delete(path);
-        File.Delete($"{file.FullName}\\{file.Name}.runtimeconfig.json");
-        File.Delete($"{file.FullName}\\Microsoft.Windows.SDK.NET.dll");
-        File.Delete($"{file.FullName}\\WinRT.Runtime.dll");
+        File.Delete($"{file.FullName}{Path.DirectorySeparatorChar}{file.Name}.runtimeconfig.json");
+        File.Delete($"{file.FullName}{Path.DirectorySeparatorChar}Microsoft.Windows.SDK.NET.dll");
+        File.Delete($"{file.FullName}{Path.DirectorySeparatorChar}WinRT.Runtime.dll");
     }
 
     foreach (var fileInfo in new DirectoryInfo(Directory.GetCurrentDirectory()).GetFiles())
